a9e8c56c3ac800d569bafa74205db787765f5445
homerobracamonte-jpg <homero.bracamonte@gmail.com>
2026-06-10 08:12:30 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import pandas as pd
+import joblib
+
+model = joblib.load("modelo_churn.pkl")
+
+st.title("Predicción de Churn")
+
+tenure = st.number_input("Meses de contrato", 0, 100)
+monthly = st.number_input("Pago mensual", 0, 200)
+
+input_data = pd.DataFrame([[tenure, monthly]],
+                          columns=["tenure", "MonthlyCharges"])
+
+if st.button("Predecir"):
+    pred = model.predict(input_data)
+
+    if pred[0] == 1:
+        st.error("El cliente se va ❌")
+    else:
+        st.success("El cliente se queda ✔")
