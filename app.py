231390b049abe93ba86fbcf5728b2489fffb5206
homerobracamonte-jpg <homero.bracamonte@gmail.com>
2026-06-10 18:15:32 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,32 +2,19 @@ import streamlit as st
 import pandas as pd
 import joblib
 
+# Cargamos el archivo del modelo
 model = joblib.load("modelo_final (2).pkl")
 
 st.title("Predicción de Churn")
 
-tenure = st.number_input(
-    "Meses como cliente (tenure)",
-    min_value=0,
-    value=12
-)
+tenure = st.number_input("Meses como cliente", min_value=0, value=12)
+monthly_charges = st.number_input("Cargo mensual", min_value=0.0, value=50.0)
 
-monthly_charges = st.number_input(
-    "Cargo mensual (MonthlyCharges)",
-    min_value=0.0,
-    value=50.0
-)
-
-# Predicción
+# El botón de predecir
 if st.button("Predecir"):
-
-    input_data = pd.DataFrame(
-        [[tenure, monthly_charges]],
-        columns=["tenure", "MonthlyCharges"]
-    )
-
+    input_data = pd.DataFrame([[tenure, monthly_charges]], columns=["tenure", "MonthlyCharges"])
     pred = model.predict(input_data)
-
+    
     if pred[0] == 1:
         st.error("El cliente se va ❌")
     else:
