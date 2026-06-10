e638c4e2e7dbfb947939a451670310bb9022c9b0
homerobracamonte-jpg <homero.bracamonte@gmail.com>
2026-06-10 12:54:42 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,33 +1,12 @@
 import streamlit as st
-import pandas as pd
 import joblib
 
 model = joblib.load("modelo_final (2).pkl")
 
-st.title("Predicción de Churn")
+st.write("Tipo de modelo:", type(model))
 
-tenure = st.number_input(
-    "Meses como cliente (tenure)",
-    min_value=0,
-    value=12
-)
-
-monthly_charges = st.number_input(
-    "Cargo mensual (MonthlyCharges)",
-    min_value=0.0,
-    value=50.0
-)
-
-if st.button("Predecir"):
-
-    input_data = pd.DataFrame(
-        [[monthly_charges, tenure]],
-        columns=["MonthlyCharges", "tenure"]
-    )
-
-    pred = model.predict(input_data)
-
-    if pred[0] == 1:
-        st.error("El cliente se va ❌")
-    else:
-        st.success("El cliente se queda ✔")
+if hasattr(model, "feature_names_in_"):
+    st.write("Columnas esperadas:")
+    st.write(list(model.feature_names_in_))
+else:
+    st.write("El modelo no tiene feature_names_in_")
